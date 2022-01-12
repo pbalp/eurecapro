9a08abd630ebe40cb559773921d7f5e46437be6f
pbalp <pbalp@unileon.es>
2022-01-12 11:16:36 +0000

    Creation of Spring project

---
+++ backend/ldap-backend/src/main/java/com/ldap/ldapbackend/LdapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ldap.ldapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapBackendApplication.class, args);
+	}
+
+}
